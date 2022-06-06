2cdd95747da2b8afebda84de470d71c729f7c9ec
Babitha Girija Babu <106980011+BabithaGirijaBabu@users.noreply.github.com>
2022-06-06 08:29:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parents new Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg :egg:')
-streamlit.text('🥑 Avocado toast')
-streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg ')
+streamlit.text('🥑 🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your own Fruit Smoothie 🥝🍇')
